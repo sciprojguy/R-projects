38f96d706ae974da179f988768d088a51cde5666
Chris Woodard <cwoodard@actsoft.com>
2019-07-12 18:47:40 -0400

    interim

--- WeatherMap/nws_api.R
+++ WeatherMap/nws_api.R
@@ -55,9 +55,36 @@ forecast.periodic <- function(meta) {
   }
   
   body <- content(resp, "parsed", encoding = "UTF-8", type = "application/json")
-  body  
+  print(body)  
   
-  #okay, now we build the tibble.
+  #okay, now we build the tibble.  this tibble has "nws.forecast" as its info.type attribute
+  #and it has 
+  # point <- body$geometry$geometries[1]
+  # polygon <- body$geometry$geometries[2]
+  # last.updated <- body$properties$updated
+  # units <- body$properties$units
+  # generator <- body$properties$forecastGenerator
+  # generated <- body$properties$generatedAt
+  # update.time <- body$properties$updateTime
+  # valid.start <- body$properties$validTimes  ... needs to be parsed into two datetime objects
+  # valid.end <- body$properties$validTimes  ... needs to be parsed into two datetime objects
+  # elevation <- body$properties$elevation$value
+  # unit.code <- body$properties$elevation$unitCode
+  
+  # forecast.periods <- body$properties$periods unrolled
+  for( period in body$properties$periods ) {
+    # number <- period$number
+    # name <- period$name
+    # start.time <- period$startTime
+    # end.time <- period$endTime
+    # is.daytime <- period$isDaytime
+    # temperature <- period$temperature
+    # temperature.unit <- period$temperatureUnit
+    # wind.speed <- period$windSpeed
+    # wind.direction <- period$windDirection
+    # short.forecast <- period$shortForecast
+    # detailed.forecast <- period$detailedForecast
+  }
 }
 
 forecast.hourly <- function(meta) {
@@ -66,10 +93,16 @@ forecast.hourly <- function(meta) {
   }
 }
 
+#this is the money shot - here we get a tibble with a lot of vectors with
+#named rows (named with date/time of forecasted values)
+
 forecast.gridpoints <- function(meta) {
   if("nws.meta" != attr(meta, "info.type")) {
     print("BLEAH")
   }
+  
+  #forecast.gridpoints results have an "info.type" attribute of "grid.data"
+  #
 }
 
 tb.meta.info <- weather.meta(28.0, -82.0)
